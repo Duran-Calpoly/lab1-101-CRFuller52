cf2b1deb0b880dff2bd6fd89f20635bcae714bce
CRFuller52 <coleryanfuller75@gmail.com>
2026-01-15 00:35:49 +0000

    done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,8 @@
-pass
+def calculate_average(num1, num2, num3):
+    average1=(num1 + num2 + num3)/3
+    return average1
+def add_tax(bill_total):
+    calculate1=bill_total + bill_total*.1
+    return calculate1
+def greet_user(name):
+    return "Hello " +name
